f8ba9d364b1ddd47858df381ec01d673fe74b27c
Valentin Roland <valentin@vroland.de>
2018-09-14 14:37:12 +0200

    use structopt

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,25 @@
 
 extern crate mediawiki_parser;
 extern crate serde_yaml;
-extern crate argparse;
+#[macro_use]
+extern crate structopt;
 
 use std::io;
 use std::io::prelude::*;
 use std::fs;
-use std::path::Path;
+use std::path::PathBuf;
 use std::process;
-use argparse::{ArgumentParser, StoreTrue, Store};
+use structopt::StructOpt;
 
-macro_rules! DESCRIPTION {
-() => (
-"This program takes MediaWiki source code and produces
- a yaml syntax tree on stdout."
-)
+
+#[derive(Debug, StructOpt)]
+/// This program takes MediaWiki source code and produces
+/// a yaml syntax tree on stdout.
+struct Args {
+    /// Path to the input file.
+    /// If none is provided, stdin is used.
+    #[structopt(short = "i", long = "input", parse(from_os_str))]
+    pub input_file: Option<PathBuf>,
 }
 
 /// read contents of a `io::Reader` into a string
@@ -36,8 +41,9 @@ fn read_from_reader(reader: &mut io::Read) -> String {
 
 
 /// Read a file from disk and store to string.
-fn read_file(filename: &str) -> String {
-    let mut file = fs::File::open(Path::new(filename)).expect("Could not open file!");
+fn read_file(filename: &PathBuf) -> String {
+    let mut file = fs::File::open(filename)
+        .expect("Could not open file!");
     read_from_reader(&mut file)
 }
 
@@ -48,35 +54,12 @@ fn read_stdin() -> String {
 }
 
 fn main() {
-    let mut use_stdin = false;
-    let mut input_file = "".to_string();
-    {
-        let mut ap = ArgumentParser::new();
-        ap.set_description(DESCRIPTION!());
-        ap.refer(&mut use_stdin).add_option(
-            &["-s", "--stdin"],
-            StoreTrue,
-            "Use stdin as input file",
-        );
-
-        ap.refer(&mut input_file).add_option(
-            &["-i", "--input"],
-            Store,
-            "Path to the input file",
-        );
-
-        ap.parse_args_or_exit();
-    }
-
-    let input: String;
-    if use_stdin {
-        input = read_stdin();
-    } else if !input_file.is_empty() {
-        input = read_file(&input_file);
+    let args = Args::from_args();
+    let input = if let Some(path) = args.input_file {
+        read_file(&path)
     } else {
-        eprintln!("No input source specified!");
-        process::exit(1);
-    }
+        read_stdin()
+    };
 
     let result = mediawiki_parser::parse(&input);
 
